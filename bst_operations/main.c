7d11f2e3c6663e2289847624dd23ed8236744218
Abraham <abraham_xu@foxmail.com>
2020-05-20 20:46:36 +0800

    bst_operation passed all tests.

--- bst_operations/main.c
+++ bst_operations/main.c
@@ -21,22 +21,24 @@ Position FindMax( BinTree BST );
 
 int main()
 {
+    FILE *fp;
+    fp = fopen("input1.txt", "r");
     BinTree BST, MinP, MaxP, Tmp;
     ElementType X;
     int N, i;
 
-    BST = NULL;
-    scanf("%d", &N);
+    BST =  NULL;
+    fscanf(fp, "%d", &N);
     for ( i=0; i<N; i++ ) {
-        scanf("%d", &X);
+        fscanf(fp, "%d", &X);
         BST = Insert(BST, X);
     }
     printf("Preorder:"); PreorderTraversal(BST); printf("\n");
     MinP = FindMin(BST);
     MaxP = FindMax(BST);
-    scanf("%d", &N);
+    fscanf(fp, "%d", &N);
     for( i=0; i<N; i++ ) {
-        scanf("%d", &X);
+        fscanf(fp, "%d", &X);
         Tmp = Find(BST, X);
         if (Tmp == NULL) printf("%d is not found\n", X);
         else {
@@ -45,33 +47,34 @@ int main()
             if (Tmp==MaxP) printf("%d is the largest key\n", Tmp->Data);
         }
     }
-    scanf("%d", &N);
+    fscanf(fp, "%d", &N);
     for( i=0; i<N; i++ ) {
-        scanf("%d", &X);
+        fscanf(fp, "%d", &X);
         BST = Delete(BST, X);
     }
     printf("Inorder:"); InorderTraversal(BST); printf("\n");
 
+    fclose(fp);
     return 0;
 }
 /* 你的代码将被嵌在这里 */
 
 void PreorderTraversal( BinTree BT ) {
     if (BT == NULL)
-        return 0;
+        return;
+
+    printf("%d ", BT->Data);
     
     if (BT->Left != NULL)
         PreorderTraversal(BT->Left);
 
     if (BT->Right != NULL)
         PreorderTraversal(BT->Right);
-
-    printf("%d ", BT->Data);
 }
 
 void InorderTraversal( BinTree BT ) {
     if (BT == NULL)
-        return 0;
+        return;
     
     if (BT->Left != NULL)
         InorderTraversal(BT->Left);
@@ -99,15 +102,65 @@ BinTree Insert( BinTree BST, ElementType X ) {
 }
 
 BinTree Delete( BinTree BST, ElementType X ) {
+    if (BST == NULL) {
+        printf("Not Found\n");
+        return NULL;
+    }
+
     if (X < BST->Data) {
         BST->Left = Delete(BST->Left, X);
     } else if (X > BST->Data) {
         BST->Right = Delete(BST->Right, X);
     } else {
-        
+        if (BST->Left == NULL && BST->Right == NULL) {
+            BST = NULL;
+        } else if (BST->Left != NULL && BST->Right == NULL) {
+            Position Temp = BST;
+            BST = BST->Left;
+            free(Temp);
+        } else if (BST->Left == NULL && BST->Right != NULL) {
+            Position Temp = BST;
+            BST = BST->Right;
+            free(Temp);
+        } else { // BST->Left != NULL && BST->Right != NULL
+            Position NewRoot = FindMax(BST->Left);
+            BST->Left = Delete(BST->Left, NewRoot->Data);
+            BST->Data = NewRoot->Data;
+        }
+    }
+
+    return BST;
+}
+ 
+Position Find( BinTree BST, ElementType X ) {
+    if (BST == NULL)
+        return NULL;
+
+    if (X < BST->Data) {
+        return Find(BST->Left, X);
+    } else if (X > BST->Data) {
+        return Find(BST->Right, X);
+    } else {
+        return BST;
     }
 }
 
-Position Find( BinTree BST, ElementType X );
-Position FindMin( BinTree BST );
-Position FindMax( BinTree BST );
+Position FindMin( BinTree BST ) {
+    if (BST == NULL)
+        return NULL;
+
+    if (BST->Left != NULL)
+        return FindMin(BST->Left);
+    else
+        return BST;
+}
+
+Position FindMax( BinTree BST ) {
+    if (BST == NULL)
+        return NULL;
+
+    if (BST->Right != NULL)
+        return FindMax(BST->Right);
+    else
+        return BST;
+}
